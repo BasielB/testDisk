ba725ebae8a78bd2a64d0315e593e554b276d247
BasielB <54147955+BasielB@users.noreply.github.com>
2021-08-02 16:48:43 +0300

    Update disks.py

--- test_disk/main/shell_logic/disks.py
+++ test_disk/main/shell_logic/disks.py
@@ -10,6 +10,12 @@ def get_all_disks():
     rows = shell_result.stdout.decode('utf-8').split('\n')
 
     for i in range(1, len(rows)-1):
+
+        split_row = rows[i].split()
+
+        if 'disk' in split_row:
+            print(split_row)
+
         print(rows[i].split())
 
     return all_disks
